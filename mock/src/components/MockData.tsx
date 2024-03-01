705f4dc139c1af93adcc7fde8b6548aaf0a46d75
Madeleinepoissonnier <madeleine_poissonnier@brown.edu>
2024-03-01 10:12:42 -0500

    feat: updated mock data for testing

--- mock/src/components/MockData.tsx
+++ mock/src/components/MockData.tsx
@@ -6,23 +6,27 @@ export const view_csv = new Map([
       ["Hi", "i'm", "Maddie"],
     ],
   ],
-  [
-    "filepath2.csv",
-    [
-      ["the", "dog", "barks"],
-      ["The", "cat", "meows"],
-    ],
-  ],
+  ["filepath2.csv", [[]]],
   [
     "filepath3.csv",
     [
       ["track", "and", "field"],
-      ["water", "polo", "swimming"],
+      ["water", "polo"],
     ],
   ],
+  ["filepath4.csv", [["Hi"], ["Howdy"], ["g'day"]]],
 ]);
 
 export const search_csv = new Map([
   ["2 Maddie", [["Hi", "i'm", "Maddie"]]],
   ["0 track", [["track", "and", "field"]]],
+  ["3 track", [[]]],
+  [
+    "State RI",
+    [
+      ["RI", "Providence", "USA"],
+      ["RI", "Newport", "USA"],
+    ],
+  ],
+  ["0 hello", [["hello"]]],
 ]);
